b6b46eb5392c60691535ff23044b7667cf567ebb
Sergey Chazov <che@webpractik.ru>
2020-05-21 20:37:43 +0300

    старт рыбы

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default class App extends React.Component {
+
+class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
-      </View>
-    );
+    return <div>sdf</div>
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+export default App;
